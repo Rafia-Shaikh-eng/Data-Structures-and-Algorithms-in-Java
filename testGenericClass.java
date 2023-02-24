e265ba22c2dbda1cc00e134cee9e59185a622ae0
Rafia Shaikh <64796657+Rafia-Shaikh-eng@users.noreply.github.com>
2023-02-24 11:11:28 -0800

    Add files via upload

---
+++ testGenericClass.java
@@ -0,0 +1,29 @@
+package Collections;
+//Generic class
+class anyPair<F,S>{
+    F first;
+    S second;
+    //constructor; initialize the data members during the creation of object
+    anyPair(F first, S second){
+        this.first=first;
+        this.second=second;
+    }
+    public F getFirst() {
+        return first;
+    }
+    public S getSecond() {
+        return second;
+    }
+}
+//testGeneric class
+public class testGenericClass{
+    //An enumeration (enum for short) in Java is a special data type which contains a set of predefined constants.
+    enum Month{
+        JAN,FEB,MAR,APR,MAY,JUN,JUL,AUG,SEPT,OCT,NOV,DEC;
+    }
+    public static void main(String[] args) {
+    //creating object of generic class 
+    anyPair<Month,Integer> quaidDay=new anyPair<Month,Integer>(Month.DEC, 25);
+    System.out.println(quaidDay.getFirst()+" "+quaidDay.getSecond());
+    }
+}
